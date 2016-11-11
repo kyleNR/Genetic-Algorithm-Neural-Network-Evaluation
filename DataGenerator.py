adb4bda3e67c539dd2d49dd60570668673c3d991
Nicholas Robinson <nr124@hw.ac.uk>
2016-11-11 12:50:14 +0000

    Added Data Generator to get NN training data  +Added Datagenerator.py

---
+++ DataGenerator.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Runs COCO to generate a set of Inputs and Outputs
+   Corresponding to Dim and to Fun_ID
+   Used to train NN
+"""
+import sys # in case we want to control what to run via command line args
+import time
+import numpy as np
+import fgeneric
+import bbobbenchmarks
+import os
+
+datapath = 'DataGen'
+
+dataLength = 10 #Number of sets of data
+dim = 2 #Number of Dimension
+fun_id = 1 #Function ID to use with COCO
+
+fileLocation = "FunctionData"
+
+if not os.path.exists(fileLocation):
+    os.makedirs(fileLocation)
+
+def DataLog(fun_id, dim):
+
+    filename = fileLocation + "/DATA-FunID-%d-DIM-%d" % (fun_id, dim)
+    file_ = open(filename, 'w')
+    for _ in range(dataLength):
+        xpop = 10. * np.random.rand(dim) - 5
+        line = ""
+        for num in xpop:
+            line = line + str(num) + ", "
+        line = line + str(f.evalfun(xpop)) + "\n"
+        file_.write(line)
+
+def DataLogAllFun(dim):
+    for fid in range(1,24):
+        f.setfun(*bbobbenchmarks.instantiate(fid, 1))
+        DataLog(fid, dim)
+
+f = fgeneric.LoggingFunction(datapath)
+f.setfun(*bbobbenchmarks.instantiate(fun_id, 1))
+
+#DataLog(fun_id, dim)
+DataLogAllFun(dim)
